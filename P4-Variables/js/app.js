635090e0f1c2c54066c4d90baca327591f828a30
hagglund1 <wilhag312@ihmeducation.se>
2024-09-12 13:07:15 +0200

    Create Project

--- P4-Variables/js/app.js
+++ P4-Variables/js/app.js
@@ -1,80 +1,11 @@
 let numberofbottles = 10;
 const line1 = "green bottles hanging on the wall";
-const line2 = "and if one green bottle, should acidentaly fal, there will be"
-console.log(numberofbottles);
-console.log(line1);
-console.log(numberofbottles);
-console.log(line1);
-console.log(line2);
-console.log(--numberofbottles);
-// output : 9
-console.log(line1);
-console.log(numberofbottles);
-console.log(line1);
-console.log(numberofbottles);
-console.log(line1);
-console.log(line2);
-console.log(--numberofbottles);
-// output : 9
-console.log(line1);console.log(numberofbottles);
-console.log(line1);
-console.log(numberofbottles);
-console.log(line1);
-console.log(line2);
-console.log(--numberofbottles);
-// output : 9
-console.log(line1);console.log(numberofbottles);
-console.log(line1);
-console.log(numberofbottles);
-console.log(line1);
-console.log(line2);
-console.log(--numberofbottles);
-// output : 9
-console.log(line1);console.log(numberofbottles);
-console.log(line1);
-console.log(numberofbottles);
-console.log(line1);
-console.log(line2);
-console.log(--numberofbottles);
-// output : 9
-console.log(line1);console.log(numberofbottles);
-console.log(line1);
-console.log(numberofbottles);
-console.log(line1);
-console.log(line2);
-console.log(--numberofbottles);
-// output : 9
-console.log(line1);console.log(numberofbottles);
-console.log(line1);
-console.log(numberofbottles);
-console.log(line1);
-console.log(line2);
-console.log(--numberofbottles);
-// output : 9
-console.log(line1);console.log(numberofbottles);
-console.log(line1);
-console.log(numberofbottles);
-console.log(line1);
-console.log(line2);
-console.log(--numberofbottles);
-// output : 9
-console.log(line1);console.log(numberofbottles);
-console.log(line1);
-console.log(numberofbottles);
-console.log(line1);
-console.log(line2);
-console.log(--numberofbottles);
-// output : 9
-console.log(line1);console.log(numberofbottles);
-console.log(line1);
-console.log(numberofbottles);
-console.log(line1);
-console.log(line2);
-console.log(--numberofbottles);
-// output : 9
+const line2 = "and if one green bottle, should accidentally fall, there will be"
 
-
-
-
-
-
+console.log(numberofbottles);
+console.log(line1);
+console.log(numberofbottles);
+console.log(line1);
+console.log(line2);
+console.log(--numberofbottles);
+// output : 9
