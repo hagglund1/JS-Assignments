053e29cddee4bcbac218e1ad10215db094616be5
hagglund1 <wilhag312@ihmeducation.se>
2024-09-12 15:19:56 +0200

    page 1

---
+++ P5-OpperatorsA/js/app.js
@@ -0,0 +1,25 @@
+let kmh = 0;
+kmh = 72;
+const result = 3.6;            //min egna
+console.log(kmh / result);
+
+let speedinput = 72
+let expectedspeed = 0
+expectedspeed = speedinput / 3.6;
+console.log(expectedspeed);
+
+let minutesinput = 3;
+let secondoutput = 0;
+secondoutput = minutesinput * 60;
+console.log(secondoutput);
+
+//let input = 11;
+//let output = 0;
+//output = input / 4
+//console.log(output);
+
+let input = 11;
+let output = 0;
+output = input % 4;
+console.log(output);
+
