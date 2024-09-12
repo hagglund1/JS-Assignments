3cfa6746c0fbab1211ad82628443ddabdefc6592
hagglund1 <wilhag312@ihmeducation.se>
2024-09-12 13:58:28 +0200

    P5- modules

---
+++ P5-Operators/js/app.js
@@ -0,0 +1,24 @@
+let x = 5 + 5; // x = 10
+let y = x + 5; // y = 15
+let z = y + y; // z = 30
+console.log(z + z); // 60
+
+let firstnumber = 1;
+let secondnumber = 2;
+let answer = 0;
+
+answer = firstnumber + secondnumber;
+console.log(firstnumber + secondnumber);
+
+answer = firstnumber - secondnumber;
+console.log(answer);
+
+answer = firstnumber * secondnumber;
+console.log(answer);
+
+answer = firstnumber / secondnumber;
+console.log(answer);
+
+answer = firstnumber % secondnumber;
+console.log(answer);
+
