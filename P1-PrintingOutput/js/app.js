4d60d0dfbaf6effbe54ea0a2076465927822f5ff
hagglund1 <wilhag312@ihmeducation.se>
2024-09-12 10:46:51 +0200

    vettefan vad dehär är

--- P1-PrintingOutput/js/app.js
+++ P1-PrintingOutput/js/app.js
@@ -1,3 +1,23 @@
 console.log ("Hello World!");
-console.log ("My name is William");
+console.log ("My name is William"); // förklarar mitt namn
+console.log ("Whats a matterbaby?");// jag är störd
+console.log ("nothing baby, whats the matter w u?");//troll
+
+const x = 5;
+const hoursinclass =5;
+const hoursofsleep =7;
+let total=0;
+
+console.log(hoursinclass);
+console.log(hoursofsleep);
+console.log(hoursofsleep + hoursofsleep);
+total = 0
+
+
+
+
+
+
+
+
 
