47d2b219382ef2783170d75e9da12281d9bbc67f
hagglund1 <wilhag312@ihmeducation.se>
2024-09-16 11:05:24 +0200

    boolean

---
+++ P-7Boolean/js/app.js
@@ -0,0 +1,11 @@
+const birthYear = 2000;
+const currentYear =2024;
+let ageOfPerson = currentYear - birthYear;
+let isChild = ageOfPerson<= 10;
+let isTeenager = ageOfPerson>=11 && ageOfPerson<=17;
+let isAdult = ageOfPerson>=18;
+
+console.log(isChild)
+console.log(isTeenager)
+console.log(isAdult)
+
