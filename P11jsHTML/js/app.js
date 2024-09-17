71d0aa59a27745a455253562910a7f2cf27fbb26
hagglund1 <wilhag312@ihmeducation.se>
2024-09-17 14:29:25 +0200

    update

---
+++ P11jsHTML/js/app.js
@@ -0,0 +1,14 @@
+const textField0 = document.getElementById("text0");
+textField0.innerHTML = "William";
+
+const headerText5 = document.getElementById("header5");
+headerText5.innerHTML = "This ma website ya idiot";
+
+headerText5.style.color = "darkgreen";
+
+const headerText6 = document.getElementById("header6");
+headerText6.innerHTML = "Darkweb";
+
+headerText6.style.color = "red";
+
+
