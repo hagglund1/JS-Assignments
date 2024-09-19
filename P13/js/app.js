973055c8bef06c63278ab80a8c25face14c4c7e7
hagglund1 <wilhag312@ihmeducation.se>
2024-09-19 10:47:27 +0200

    Refactor to improve modular

--- P13/js/app.js
+++ P13/js/app.js
@@ -1,8 +1,16 @@
-console.log('Hello World!')
+//Document Elements
 //looks for text 0, assigns it to variable calls textField0
 const textField0 = document.getElementById('text0');
-textField0.innerHTML = showStylizedScore(5);
+const textField1 = document.getElementById('text1');
 
+// Internal Variables
+let score = 0;
+
+// Process
+
+increaseScoreByOne();
+
+//controllers
 function sendScore (inputNumber){
   return inputNumber * 100 //output is a number
 }
@@ -11,4 +19,15 @@ function showStylizedScore (scoreNumber){
   return "your score is " + scoreNumber * 100;
 }
 
+function increaseScoreByOne ( ) {
+  score++;
+  updateScoreText(score);
+}
 
+//view
+function updateScoreText (newText) {
+  textField0.innerHTML = newText;
+}
+function updateStatus (newText){
+  textField0.innerHTML = newText;
+}
